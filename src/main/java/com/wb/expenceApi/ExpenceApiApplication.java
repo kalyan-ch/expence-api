a50a6981350e1ef2f94500bd3be67fa249ade0aa
Kalyan Chavali <kalyan.chavali123@gmail.com>
2024-09-30 15:41:35 -0700

    initial commit

---
+++ src/main/java/com/wb/expenceApi/ExpenceApiApplication.java
@@ -0,0 +1,13 @@
+package com.wb.expenceApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenceApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenceApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wb/expenceApi/ExpenceApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wb.expenceApi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenceApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
